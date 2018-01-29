f05044c69f64f390b25d949622559af1773cab85
scau_zhangpeng <scau_zhangpeng@163.com>
2018-01-29 09:28:20 +0800

    [新增开发] 1.新增：包相关工具类 2.修改：版本号以及关于界面的UI

--- app/src/main/java/org/scau/riotgame/setting/AboutRiotGameActivity.java
+++ app/src/main/java/org/scau/riotgame/setting/AboutRiotGameActivity.java
@@ -2,9 +2,16 @@ package org.scau.riotgame.setting;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
-import org.scau.riotgame.base.BaseActivity;
+import com.xyz.basiclib.PackageUtil;
+
 import org.scau.riotgame.R;
+import org.scau.riotgame.base.BaseActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by ZP on 2017/7/30.
@@ -15,10 +22,28 @@ import org.scau.riotgame.R;
 
 public class AboutRiotGameActivity extends BaseActivity {
 
+    @Bind(R.id.tv_version_info)
+    TextView mTvVersionInfo;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_roit_game);
+        ButterKnife.bind(this);
         initTopBar(this);
+
+        showVersionInfo();
+    }
+
+    private void showVersionInfo() {
+        String appVersionName = PackageUtil.getAppVersionName(this);
+        int appVersionCode = PackageUtil.getAppVersionCode(this);
+        String versionInfo = getString(R.string.version_info, appVersionName, appVersionCode);
+        mTvVersionInfo.setText(versionInfo);
+    }
+
+    @OnClick(R.id.btn_update_version)
+    public void updateVersion() {
+
     }
 }
---
+++ basiclib/src/main/java/com/xyz/basiclib/WrapperPackageInfo.java
@@ -0,0 +1,84 @@
+package com.xyz.basiclib;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.graphics.drawable.Drawable;
+import android.text.format.Formatter;
+
+/**
+ * Created by ZP on 2017/12/5.
+ */
+
+public class WrapperPackageInfo {
+
+    private PackageInfo mPackageInfo;
+
+    private String appName;
+
+    private boolean isSystemApp = false;
+
+    private Drawable mDrawable;
+
+    private long appSize;
+
+    public WrapperPackageInfo(PackageInfo packageInfo, String appName, boolean isSystemApp, Drawable drawable, long appSize) {
+        mPackageInfo = packageInfo;
+        this.appName = appName;
+        this.isSystemApp = isSystemApp;
+        mDrawable = drawable;
+        this.appSize = appSize;
+    }
+
+    public Drawable getDrawable() {
+        return mDrawable;
+    }
+
+    public void setDrawable(Drawable drawable) {
+        mDrawable = drawable;
+    }
+
+    public PackageInfo getPackageInfo() {
+        return mPackageInfo;
+    }
+
+    public void setPackageInfo(PackageInfo packageInfo) {
+        mPackageInfo = packageInfo;
+    }
+
+    public boolean isSystemApp() {
+        return isSystemApp;
+    }
+
+    public void setSystemApp(boolean systemApp) {
+        isSystemApp = systemApp;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public long getAppSize() {
+        return appSize;
+    }
+
+    public void setAppSize(long appSize) {
+        this.appSize = appSize;
+    }
+
+    public String formatAppSize(Context context) {
+        return Formatter.formatFileSize(context, this.appSize);
+    }
+
+    @Override
+    public String toString() {
+        return "WrapperPackageInfo{" +
+                "mPackageInfo=" + mPackageInfo +
+                ", appName='" + appName + '\'' +
+                ", isSystemApp=" + isSystemApp +
+                '}';
+    }
+}
