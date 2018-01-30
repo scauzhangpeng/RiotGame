5cc9ca4c58b321cab7d6d5fd9fe15727c78d11aa
scau_zhangpeng <scau_zhangpeng@163.com>
2018-01-30 17:07:03 +0800

    [新增开发] 1.新增：屏幕操作类 2.新增：权限检查基类 3.新增：基本对话框

---
+++ basiclib/src/main/java/com/xyz/basiclib/DefaultDialog.java
@@ -0,0 +1,68 @@
+package com.xyz.basiclib;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.StyleRes;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by ZP on 2018/1/30.
+ */
+
+public class DefaultDialog extends Dialog {
+    private static final String TAG = "DefaultDialog";
+    private Context context;
+
+    public DefaultDialog(@NonNull Context context) {
+        super(context, R.style.DefaultDialog);
+        this.context = context;
+    }
+
+    public DefaultDialog(@NonNull Context context, @StyleRes int themeResId) {
+        super(context, themeResId);
+        this.context = context;
+    }
+
+    protected DefaultDialog(@NonNull Context context, boolean cancelable, @Nullable OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+        this.context = context;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
+        View dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_default, null);
+        setContentView(dialogView);
+
+        Window win = getWindow();
+        WindowManager.LayoutParams lp = win.getAttributes();
+        lp.gravity = Gravity.CENTER;
+        int screenWidth = ScreenUtil.getScreenWidth(context);
+        int paddingBothSize = ScreenUtil.dp2px(context, 80);
+        int dialogWidth = screenWidth - paddingBothSize;
+        lp.height = dialogWidth / 2;
+        lp.width = dialogWidth;
+        win.setAttributes(lp);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+}
---
+++ basiclib/src/main/java/com/xyz/basiclib/mvp/CheckPermissionActivity.java
@@ -0,0 +1,13 @@
+package com.xyz.basiclib.mvp;
+
+/**
+ * Created by ZP on 2018/1/30.
+ * <p>
+ * 检查权限
+ * </p>
+ */
+
+public abstract class CheckPermissionActivity extends BaseActivity {
+
+
+}
