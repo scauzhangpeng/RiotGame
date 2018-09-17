2e26259ce823546b23feb5e22a85ffe61a0c7a8a
scau_zhangpeng <scau_zhangpeng@163.com>
2018-09-17 13:08:13 +0800

    [基础模块] 1.新增：阿里路由

--- app/src/main/java/org/scau/riotgame/RiotGameApplication.java
+++ app/src/main/java/org/scau/riotgame/RiotGameApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.util.Log;
 
+import com.alibaba.android.arouter.launcher.ARouter;
+
 /**
  * Created by ZP on 2017/8/8.
  */
@@ -19,6 +21,16 @@ public class RiotGameApplication extends Application {
         Log.i(TAG, "onCreate: ");
         super.onCreate();
         sContext = getApplicationContext();
+        initARouter();
+    }
+
+    private void initARouter() {
+        if (BuildConfig.DEBUG) {
+            // These two lines must be written before init, otherwise these configurations will be invalid in the init process
+            ARouter.openLog();     // Print log
+            ARouter.openDebug();   // Turn on debugging mode (If you are running in InstantRun mode, you must turn on debug mode! Online version needs to be closed, otherwise there is a security risk)
+        }
+        ARouter.init(this); // As early as possible, it is recommended to initialize in the Application
     }
 
     public static Context getContext() {
