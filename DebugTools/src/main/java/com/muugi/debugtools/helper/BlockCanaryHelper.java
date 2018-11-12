be4c7d5ccc65533e86e9595a2b4dd86888725f20
zhangpeng <scau_zhangpeng@163.com>
2018-11-12 11:23:38 +0800

    [优化] 1.优化：gradle 2.优化：测试工具初始化调用

---
+++ DebugTools/src/main/java/com/muugi/debugtools/helper/BlockCanaryHelper.java
@@ -0,0 +1,55 @@
+package com.muugi.debugtools.helper;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.github.moduth.blockcanary.BlockCanary;
+import com.muugi.debugtools.block.AppBlockCanaryContext;
+import com.muugi.debugtools.ele.EleActivityLifecycleCallbacks;
+import com.squareup.leakcanary.LeakCanary;
+import com.tencent.wstt.gt.controller.GTRController;
+
+/**
+ * @author zhangpeng
+ * @date 2018/11/12
+ * @since 1.0
+ */
+public class BlockCanaryHelper {
+
+
+    /**
+     * 初始化GT性能检测工具.
+     */
+    public void initGT(Context context) {
+        GTRController.init(context);
+    }
+
+    /**
+     * 初始化页面卡顿检测工具.
+     */
+    public void initBlockCanary(Context context) {
+        BlockCanary.install(context, new AppBlockCanaryContext()).start();
+    }
+
+    /**
+     * 初始化内存泄漏检测工具.
+     */
+    public void initLeakCanary(Application context) {
+        if (LeakCanary.isInAnalyzerProcess(context)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(context);
+    }
+
+    /**
+     * 初始化饿了么UE调试工具.
+     */
+    public void initEleUeTool(Application context) {
+//        UETool.putFilterClass(FilterOutView.class);
+//        UETool.putAttrsProviderClass(CustomAttribution.class);
+
+        context.registerActivityLifecycleCallbacks(new EleActivityLifecycleCallbacks());
+    }
+}
--- app/src/main/debug/muugi/DebugApplication.java
+++ app/src/main/debug/muugi/DebugApplication.java
@@ -2,11 +2,7 @@ package muugi;
 
 import android.util.Log;
 
-import com.github.moduth.blockcanary.BlockCanary;
-import com.muugi.debugtools.block.AppBlockCanaryContext;
-import com.muugi.debugtools.ele.EleActivityLifecycleCallbacks;
-import com.squareup.leakcanary.LeakCanary;
-import com.tencent.wstt.gt.controller.GTRController;
+import com.muugi.debugtools.helper.BlockCanaryHelper;
 
 import org.scau.riotgame.RiotGameApplication;
 
@@ -17,12 +13,13 @@ import org.scau.riotgame.RiotGameApplication;
 public class DebugApplication extends RiotGameApplication {
 
     private static final String TAG = "muugi.DebugApplication";
+    private BlockCanaryHelper mHelper;
 
     @Override
     public void onCreate() {
         Log.d(TAG, "onCreate: ");
         super.onCreate();
-
+        mHelper = new BlockCanaryHelper();
         initEleUeTool();
         initLeakCanary();
         initBlockCanary();
@@ -33,36 +30,28 @@ public class DebugApplication extends RiotGameApplication {
      * 初始化GT性能检测工具.
      */
     private void initGT() {
-        GTRController.init(this);
+        mHelper.initGT(this);
     }
 
     /**
      * 初始化页面卡顿检测工具.
      */
     private void initBlockCanary() {
-        BlockCanary.install(this, new AppBlockCanaryContext()).start();
+        mHelper.initBlockCanary(this);
     }
 
     /**
      * 初始化内存泄漏检测工具.
      */
     private void initLeakCanary() {
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
+        mHelper.initLeakCanary(this);
     }
 
     /**
      * 初始化饿了么UE调试工具.
      */
     private void initEleUeTool() {
-//        UETool.putFilterClass(FilterOutView.class);
-//        UETool.putAttrsProviderClass(CustomAttribution.class);
-
-        registerActivityLifecycleCallbacks(new EleActivityLifecycleCallbacks());
+        mHelper.initEleUeTool(this);
     }
 
 }
